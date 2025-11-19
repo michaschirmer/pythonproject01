12ba08a9ddadd237f77b7809c33154b4ebdabd29
Micha Schirmer <michajava8@gmail.com>
2025-11-19 22:46:32 +0100

    main.py import b

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from modul_a import plus
+from modul_b import min
 
 def main():
     print("Hallo, Python-Welt!")
     # hier kommt deine Programm-Logik hin
     print(plus(2,3))
+    print(min(2,3))
 
 if __name__ == "__main__":
     main()
